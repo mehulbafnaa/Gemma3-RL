2d037daa1594eacd80d255d5738e5a2c0da17319
Mehul Bafna <74257240+mehulbafnaa@users.noreply.github.com>
2025-05-13 17:11:43 -0400

    Update download_model.sh

--- pre-trained/download_model.sh
+++ pre-trained/download_model.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Export your Kaggle username and API key
-export KAGGLE_USERNAME=your_username
-
-export KAGGLE_KEY=your_API_key
+export KAGGLE_USERNAME=<YOUR USERNAME>
+export KAGGLE_KEY=<YOUR KAGGLE KEY>
 
 curl -L -u $KAGGLE_USERNAME:$KAGGLE_KEY\
-  -o ./model.tar.gz\
+  -o ~/Downloads/model.tar.gz\
   https://www.kaggle.com/api/v1/models/google/gemma-3/flax/gemma3-4b-it/1/download
-
-
